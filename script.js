a01dd4e4151b577766740f57486cea7891a91d74
vpasamic <70293668+vpasamic@users.noreply.github.com>
2020-09-23 13:43:27 -0700

    Remotiveapiquerys (#10)
    * added necessary variables from remotive api
    
    * removed hello added script
    
    Co-authored-by: git99-src <70079158+git99-src@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ $("#search").on("click", function(){
     url: query,
     method:"GET"
    }) .then(function(remotive){
-       
        for(var i=0; i < 8; i++ ){
         var title= remotive.jobs[i].title
         var tags=remotive.jobs[i].tags
         var url=remotive.jobs[i].url
+
+        var company=remotive.jobs[i].company_name
         console.log(title)
         console.log(tags)
         console.log(url)
+        console.log(company)
+
        } 
    })
 
