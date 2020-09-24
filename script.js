363a54122e560504d1e6b37514c4e56c9cd3a0b2
vpasamic <70293668+vpasamic@users.noreply.github.com>
2020-09-24 07:31:59 -0700

    added clear button and responsive dom element creators when using search button (#17)

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+$("#clear").on("click", function(){
+   $("#mainbody").empty()
+})
 $("#search").on("click", function(){
-   var query=  "https://cors-anywhere.herokuapp.com/https://remotive.io/api/remote-jobs?category=software-dev"
-   $.ajax({
-    url: query,
-    method:"GET"
-   }) .then(function(remotive){
-       for(var i=0; i < 8; i++ ){
-        var title= remotive.jobs[i].title
-        var tags=remotive.jobs[i].tags
-        var url=remotive.jobs[i].url
-        var company=remotive.jobs[i].company_name
+  var query=  "https://cors-anywhere.herokuapp.com/https://remotive.io/api/remote-jobs?category=software-dev"
+  $.ajax({
+   url: query,
+   method:"GET"
+  }) .then(function(remotive){
+      
+      for(var i=0; i < 8; i++ ){
+          var a=i+1
+       var title= remotive.jobs[i].title
+       var tags=remotive.jobs[i].tags
+       var url=remotive.jobs[i].url
+       var company=remotive.jobs[i].company_name
 
-        console.log(title)
-        console.log(tags)
-        console.log(url)
-        console.log(company)
+       console.log(title)
+       console.log(tags)
+       console.log(url)
+       console.log(company)
+     
+     //create card to append to mainbody
+      var id="card"+a
+      var idcont="cardcontent"+a
+      
+      $("#mainbody").append("<div id="+id+"></div>")
+      var card=$("#"+id)
+      card.addClass("card")
+      card.append("<div id="+idcont+"></div>")
+      var cardcontent=$("#"+idcont)
+      cardcontent.addClass("card-content")
 
-        // creates job title header for cards
-        var titleEl=$("<h2>")
-        titleEl.text(title)
-        //change#mainbody later
-        $("#mainbody").append(titleEl)
+       // creates job title header for cards
+          var titleEl=$("<p>")
+       titleEl.addClass("title")
+      titleEl.text(title)
+      
+       cardcontent.append(titleEl)
 
-        //creates url element
-        var urlEl=$("<a>")
-        urlEl.attr("href", url)
+        //create company el
+      var companyEl=$("<p>").addClass("company")
+      companyEl.text(company)
+      cardcontent.append(companyEl)
+
+       //creates url element
+       var urlEl=$("<a>")
+       urlEl.attr("href", url)
         urlEl.text(url)
-           //change#mainbody later
-        $("#mainbody").append(urlEl)
+       cardcontent.append(urlEl)
 
+         //create footer
+        var footerEl=$("<footer>").addClass("card-footer")
+        cardcontent.append(footerEl)
 
-        // creates tags buttons
-        if(tags.length!=0){
-            for(var x=0; x<tags.length; x++){
-            tagsbtn=$("<button>")
-            tagsbtn.text(tags[x])
-            tagsbtn.attr("data-type",tags[x])
-               //change#mainbody later
-            $("#mainbody").append(tagsbtn)
-        }}
+      // creates tags link
+       if(tags.length!=0){
+           for(var x=0; x<tags.length; x++){
+           var footeritem=$("<p>").addClass("card-footer-item")
+          footeritem.append(footerEl)
+           var tagslink=$("<span>")
+           tagslink.text(tags[x])
+          tagslink.append(footerEl)
+       }}
 
-       } 
-   })
+      } 
+  })
 
 })
