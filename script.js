0d9faef8385a2e7287b28b50e0ed0ec30465dac3
git99.src <cpu.al2@gmail.com>
2020-09-24 17:19:37 -0700

    Issue Tile format and DOM employment type addressed

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ $("#search").on("click", function(){
    method:"GET"
   }) .then(function(remotive){
       
-      for(var i=0; i < 15; i++ ){
+      for(var i=0; i < 10; i++ ){
           var a=i+1
        var title= remotive.jobs[i].title
        var tags=remotive.jobs[i].tags
        var url=remotive.jobs[i].url
        var company=remotive.jobs[i].company_name
-     
+       var jobtype = remotive.jobs[i].job_type
+
+       if (jobtype === "full_time") {
+        jobtype = "Full Time";
+      } else if (jobtype === "contract") {
+        jobtype = "Contract";
+      }
+       
+      
     //create new tiles to append to mainbody
     var tileid="tile-"+a
     $("#mainbody").append("<div id="+tileid+"></div>")
@@ -30,20 +38,25 @@ $("#search").on("click", function(){
 
        // creates job title header for cards
           var titleEl=$("<p>")
-       titleEl.addClass("title")
+       titleEl.addClass("title is-4")
       titleEl.text(title)
       
        cardcontent.append(titleEl)
 
         //create company el
       var companyEl=$("<span>").addClass("company")
-      companyEl.html("<p>"+company+"</p>")
+      companyEl.html("<p>Company: "+company+"</p>")
+      cardcontent.append(companyEl)
+      
+      //creates a job type
+      var companyEl=$("<span>").addClass("jobtype")
+      companyEl.html("<p>Job Type: "+jobtype+"</p>")
       cardcontent.append(companyEl)
 
        //creates url element
        var urlEl=$("<a>")
        urlEl.attr("href", url)
-        urlEl.text(url)
+        urlEl.text("Click here to view full job description")
        cardcontent.append(urlEl)
 
          //create footer
@@ -66,7 +79,6 @@ $("#search").on("click", function(){
        tile.append("<div id="+ischildid+"></div>")
        var dropdown=$("#"+ischildid)
        dropdown.addClass("tile is-child box notification is-dark has-text-white is-hidden is-vertical")
-      
        
    } 
   })
@@ -86,8 +98,7 @@ $(document).on("click", ".company", function(event){
     url: queryURL,
     method:"GET"
    }) .then(function(newsAPI){
-    console.log(queryURL)
-    console.log(newsAPI.response.docs)
+
     if(newsAPI.response.docs.length===0){
       
       var dropDown = cardContent.siblings(".is-child")
@@ -101,7 +112,8 @@ $(document).on("click", ".company", function(event){
 
       var dropDown = cardContent.siblings(".is-child")
       dropDown.removeClass("is-hidden")
-
+      
+      // grab 3 articles related to company
       for(var j=0; j < 3; j++ ){
         var dd=$("<div>")
         dropDown.append(dd)  
@@ -110,10 +122,10 @@ $(document).on("click", ".company", function(event){
         var br=$("<br>")
         var snippet = $("<p>").text("Summary: " + dataResponse[j].snippet);
         var headline = $("<p>").text("Headline: " + dataResponse[j].headline.main);
-        
+        //url for article search
         var articleURL = $("<a>").text("Link: " + dataResponse[j].web_url).attr({'href': dataResponse[j].web_url , "target": "_blank"});
         dd.append(headline,br, snippet,br, articleURL,br, br);
-  }
+    }
   }
   
 })
